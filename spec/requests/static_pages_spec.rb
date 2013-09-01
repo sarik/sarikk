f6d4aec8ee1cf17fc8f0aba79856634730f15970
sarik <siddiquisarik@gmail.com>
2013-09-01 20:19:38 +0530

    static pages rb changed

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,48 +2,6 @@ require 'spec_helper'
 
 describe "Static pages" do
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-  describe "Home page" do
-
-    it "should have the content 'Sample App'" do
-      visit '/static_pages/home'
-      expect(page).to have_content('Sample App')
-    end
-
-    it "should have the title 'Home'" do
-      visit '/static_pages/home'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
-    end
-  end
-
-  describe "Help page" do
-
-    it "should have the content 'Help'" do
-      visit '/static_pages/help'
-      expect(page).to have_content('Help')
-    end
-
-    it "should have the title 'Help'" do
-      visit '/static_pages/help'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
-    end
-  end
-
-  describe "About page" do
-
-    it "should have the content 'About Us'" do
-      visit '/static_pages/about'
-      expect(page).to have_content('About Us')
-    end
-
-    it "should have the title 'About Us'" do
-      visit '/static_pages/about'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
-    end
-=======
-=======
->>>>>>> 271f5e7e989a220f1be3abd88ad73b736bbe772d
   subject { page }
 
   describe "Home page" do
@@ -73,9 +31,5 @@ describe "Static pages" do
 
     it { should have_content('Contact') }
     it { should have_title(full_title('Contact')) }
-<<<<<<< HEAD
->>>>>>> 271f5e7e989a220f1be3abd88ad73b736bbe772d
-=======
->>>>>>> 271f5e7e989a220f1be3abd88ad73b736bbe772d
   end
 end
