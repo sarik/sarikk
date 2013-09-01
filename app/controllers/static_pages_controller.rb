9bd9ece48836a414909f21c7af77bf3380841bf4
sarik <siddiquisarik@gmail.com>
2013-09-01 19:54:57 +0530

    initial commit

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,18 @@
+class StaticPagesController < ApplicationController
+  def home
+  end
+
+  def help
+<<<<<<< HEAD
+<<<<<<< HEAD
+=======
+  	@file=Dir.glob('*')
+>>>>>>> 271f5e7e989a220f1be3abd88ad73b736bbe772d
+=======
+  	@file=Dir.glob('*')
+>>>>>>> 271f5e7e989a220f1be3abd88ad73b736bbe772d
+  end
+
+  def about
+  end
+end
---
+++ app/helpers/application_helper.rb
@@ -0,0 +1,23 @@
+module ApplicationHelper
+<<<<<<< HEAD
+<<<<<<< HEAD
+end
+=======
+=======
+>>>>>>> 271f5e7e989a220f1be3abd88ad73b736bbe772d
+
+  # Returns the full title on a per-page basis.
+  def full_title(page_title)
+    base_title = "Ruby on Rails Tutorial Sample App"
+    if page_title.empty?
+      base_title
+    else
+      "#{base_title} | #{page_title}"
+    end
+  end
+<<<<<<< HEAD
+end
+>>>>>>> 271f5e7e989a220f1be3abd88ad73b736bbe772d
+=======
+end
+>>>>>>> 271f5e7e989a220f1be3abd88ad73b736bbe772d
